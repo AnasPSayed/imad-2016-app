a338512e4d505217b8b040c57280a0ba5538a29b
AnasPSayed <anaspsayed@gmail.com>
2016-10-06 15:26:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One - Anas P Sayed',
+    heading: 'Article One',
+    date: '6-10-2016',
+    content: `  
+        <p>
+                This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.
+            </p>
+            
+            <p>
+                This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.
+            </p>
+            
+            <p>
+                This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.This is the content of the article one.
+            </p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            ${title}
+            <link href="ui/style.css" rel="stylesheet"/>
+        </head>
+        
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">HOME</a>
+                </div>
+                <h3>
+                    ${heading}
+                    </h3>
+                <div>
+                    ${date}
+                </div>
+                ${content}
+            </div>
+        </body>
+    </html>`
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTeplate(articleOne));
 })
 
 app.get('/article-two', function (req,res){
